257e668232971b6d69759e750c6aab757e75a5ab
Nassila HAMOUCHE <nassilahamouche@gmail.com>
2021-01-28 00:56:47 +0100

    version 1

---
+++ src/src/Entity/Book.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @var  int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Author;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $PublishedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->Author;
+    }
+
+    public function setAuthor(string $Author): self
+    {
+        $this->Author = $Author;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->PublishedAt;
+    }
+
+    public function setPublishedAt(?\DateTimeInterface $PublishedAt): self
+    {
+        $this->PublishedAt = $PublishedAt;
+
+        return $this;
+    }
+}
